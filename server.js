54cfe3b7c26392d2415521431d14d1018e8473a8
hoth1771 <hoth1771@gmail.com>
2015-01-21 12:09:03 -0800

    ... so cool

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Auto Update is working!\n');
+  res.end('So Cool!\n');
 }).listen(port);
 
 /*
